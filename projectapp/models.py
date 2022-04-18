b970c377feaa321ecdf708c7f8298ccb455c00fb
조동주 <jodongju@jodongjus-MacBook-Air.local>
2022-04-18 16:49:28 +0900

    set projectapp model

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+
+    title = models.CharField(max_length=40, blank=True)
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='project', null=True)
+    description = models.CharField(max_length=300, null=True)
+    goalamount = models.IntegerField(blank=True)
+    enddate = models.DateTimeField(auto_now_add=False, blank=True)
+    funding = models.IntegerField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+
+
--- projectapp/views.py
+++ projectapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import CreateView, DetailView
+
+from projectapp.forms import ProjectCreationForm
+from projectapp.models import Project
+
+
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectCreationForm
+
+    def get_success_url(self):
+        return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
+
+
+
+
+
