bc8c82671521e27fe1a3ecb408e078e02d4b5a90
조동주 <jodongju@jodongjus-MacBook-Air.local>
2022-04-20 22:20:45 +0900

    modify projectapp model

--- accountapp/views.py
+++ accountapp/views.py
@@ -10,5 +10,5 @@ from django.views.generic import CreateView
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('projectapp:detail')
+    success_url = reverse_lazy('accountapp:loginp')
     template_name = 'accountapp/create.html'
